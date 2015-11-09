14da9d0f80f69a2d4d1916c6915735df427af037
karly sindy <karlysindy@gmail.com>
2015-11-09 11:58:57 -0500

    adding to repo

---
+++ SICER_HA1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# !!!!Replace it with your own directory!!!
+SICER=/vol1/home/sindyk/SICER/SICER_V1.1/SICER
+
+sh $SICER/SICER-rb.sh /vol1/home/sindyk/Emily/Align-1_2015-06-12/HA_1_ACATCG HA1.bed /vol1/home/sindyk/Emily/Align-1_2015-06-12/HA_1_ACATCG/HA1_SICER hg19 5 200 350 0.74 600 100
